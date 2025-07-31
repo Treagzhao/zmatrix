832789c0a24b6dfe9a63d907c2eef63d9a2652fd
Treagzhao <920866163@qq.com>
2025-07-31 18:45:43 +0800

    feat: 更新CosMatrix实现并添加基准测试 - 重构CosMatrix使用数组替代Matrix类型 - 添加bench_cos_matrix基准测试文件 - 添加random_matrix工具函数 - 更新Cargo.toml版本和基准测试配置

--- benches/bench_cos_matrix.rs
+++ benches/bench_cos_matrix.rs
@@ -36,5 +36,43 @@ fn bench_cos_matrix_product_vector(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, bench_cos_matrix_product_vector);
+
+pub fn random_f64_array_9() -> [f64; 9] {
+    let mut rng = rand::thread_rng();
+    let mut arr = [0.0; 9];
+    for item in &mut arr {
+        *item = rng.random();
+    }
+    arr
+}
+fn bench_cos_matrix_product(c: &mut Criterion) {
+    let mut group = c.benchmark_group("cos_matrix_product");
+    let arr = random_f64_array_9();
+    let cos1 = CosMatrix::new([
+        [arr[0], arr[1], arr[2]],
+        [arr[3], arr[4], arr[5]],
+        [arr[6], arr[7], arr[8]],
+    ]);
+    let arr = random_f64_array_9();
+    let cos2 = CosMatrix::new([
+        [arr[0], arr[1], arr[2]],
+        [arr[3], arr[4], arr[5]],
+        [arr[6], arr[7], arr[8]],
+    ]);
+    group.bench_with_input("cos matrix product", &(cos1, cos2), |b, (cos1, cos2)| {
+        b.iter(|| cos1.product(cos2));
+    });
+}
+
+fn bench_cos_matrix_add(c: &mut Criterion) {
+    let mut group = c.benchmark_group("cos_matrix_add");
+    let arr = random_f64_array_9();
+    let cod = CosMatrix::new([
+        [arr[0], arr[1], arr[2]],
+        [arr[3], arr[4], arr[5]],
+        [arr[6], arr[7], arr[8]],
+    ]);
+}
+
+criterion_group!(benches,bench_cos_matrix_product_vector, bench_cos_matrix_product);
 criterion_main!(benches);
